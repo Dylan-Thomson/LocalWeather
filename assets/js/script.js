fe0a2e1a6a1f405825b6d949e0221d229dd127c3
Dylan Thomson <djthomson88@gmail.com>
2017-06-18 19:43:06 -0400

    Get weather info
    -Build URL using coordinates from geolocation
    -getJSON using URL, write current weather conditions to a paragraph

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ $(".btn-primary").on("click", function() {
 	if ("geolocation" in navigator) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 		  $("#location").text(position.coords.latitude + " " + position.coords.longitude);
-			var url = "https://api.darksky.net/forecast/b2e8d595c58230947ca08220d0572147/" + position.coords.latitude + ", " + position.coords.longitude;
+			var url = "https://api.darksky.net/forecast/b2e8d595c58230947ca08220d0572147/" + position.coords.latitude + ",%20" + position.coords.longitude + "?callback=?";
+			console.log(url);
+			$.getJSON(url, function(data) {
+				console.log(data.currently);
+				$("#summary").text(data.currently.summary);
+			});
 		});
 	}
 
