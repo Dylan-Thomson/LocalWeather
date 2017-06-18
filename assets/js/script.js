37513c2407aeade665858717f36025dfb7a01a4d
Dylan Thomson <djthomson88@gmail.com>
2017-06-18 19:30:56 -0400

    Build URL for API Request

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ $(".btn-primary").on("click", function() {
 	if ("geolocation" in navigator) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 		  $("#location").text(position.coords.latitude + " " + position.coords.longitude);
+			var url = "https://api.darksky.net/forecast/b2e8d595c58230947ca08220d0572147/" + position.coords.latitude + ", " + position.coords.longitude;
 		});
 	}
+
 });
