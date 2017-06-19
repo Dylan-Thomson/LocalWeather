d99b1beaaaee0e9ce96b7f4f7da7eb79553fda14
Dylan Thomson <djthomson88@gmail.com>
2017-06-19 07:58:00 -0400

    Fahrenheit Icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(".btn-primary").on("click", function() {
 			$.getJSON(url, function(data) {
 				console.log(data.currently);
 				$("#summary").text(data.currently.summary);
-				$("#temperature").text(data.currently.apparentTemperature + "F");
+				$("#temperature").html(data.currently.apparentTemperature + "<i class='wi wi-fahrenheit'></i>");
 			});
 		});
 	}
