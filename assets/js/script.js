28c257f6a799d4b395bd8529d84c9ae3f83c2005
Dylan Thomson <djthomson88@gmail.com>
2017-06-19 10:17:07 -0400

    Celsius and Fahrenheit Conversions

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,11 @@ $(".btn-primary").on("click", function() {
 		});
 	}
 });
+
+function fahrenheitToCelsius(temp) {
+	return ((temp - 32) * 5)/9;
+}
+
+function celsiusToFahrenheit(temp) {
+	return (temp * 9)/5 + 32;
+}
