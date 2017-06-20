674d82dd41ab15e482fb18569568977d542d13fb
Dylan Thomson <djthomson88@gmail.com>
2017-06-19 20:26:07 -0400

    Remove button
    -Remove button since it is apparently no longer necessary

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
 var fahrenheit = true;
 var temperature = 78.21;
 
-$(".btn-primary").on("click", function() {
-	// Check for location data
+// $(".btn-primary").on("click", function() {
+// 	Check for location data
+// 	if ("geolocation" in navigator) {
+// 		// Get location data
+// 		navigator.geolocation.getCurrentPosition(function(position) {
+// 		  $("#location").text(position.coords.latitude + " " + position.coords.longitude);
+// 		  // Build URL
+// 			var url = "https://api.darksky.net/forecast/b2e8d595c58230947ca08220d0572147/" + position.coords.latitude + ",%20" + position.coords.longitude + "?callback=?";
+// 			console.log(url);
+
+// 			// Get weather data
+// 			$.getJSON(url, function(data) {
+// 				console.log(data.currently);
+// 				// Update page with weather data
+// 				$("#summary").text(data.currently.summary);
+// 				temperature = data.currently.apparentTemperature;
+// 				$("#temperature").html(data.currently.apparentTemperature + "<i class='wi wi-fahrenheit'></i>");
+// 			});
+// 		});
+// 	}
+
+// 	$(this).addClass("hidden");
+// });
+
 	if ("geolocation" in navigator) {
 		// Get location data
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -21,9 +43,6 @@ $(".btn-primary").on("click", function() {
 			});
 		});
 	}
-
-	// $(this).addClass("hidden");
-});
 
 
 $("#temperature").on("click", function() {
