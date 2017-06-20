7ffc91846b302733f5129d4199e50d97b56f9d50
Dylan Thomson <djthomson88@gmail.com>
2017-06-19 20:48:16 -0400

    Cleaup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,6 @@
 var fahrenheit = true;
 var temperature = 78.21;
 
-// $(".btn-primary").on("click", function() {
-// 	Check for location data
-// 	if ("geolocation" in navigator) {
-// 		// Get location data
-// 		navigator.geolocation.getCurrentPosition(function(position) {
-// 		  $("#location").text(position.coords.latitude + " " + position.coords.longitude);
-// 		  // Build URL
-// 			var url = "https://api.darksky.net/forecast/b2e8d595c58230947ca08220d0572147/" + position.coords.latitude + ",%20" + position.coords.longitude + "?callback=?";
-// 			console.log(url);
-
-// 			// Get weather data
-// 			$.getJSON(url, function(data) {
-// 				console.log(data.currently);
-// 				// Update page with weather data
-// 				$("#summary").text(data.currently.summary);
-// 				temperature = data.currently.apparentTemperature;
-// 				$("#temperature").html(data.currently.apparentTemperature + "<i class='wi wi-fahrenheit'></i>");
-// 			});
-// 		});
-// 	}
-
-// 	$(this).addClass("hidden");
-// });
-
-	if ("geolocation" in navigator) {
-		// Get location data
-		navigator.geolocation.getCurrentPosition(function(position) {
-		  $("#location").text(position.coords.latitude + " " + position.coords.longitude);
-		  // Build URL
-			var url = "https://api.darksky.net/forecast/b2e8d595c58230947ca08220d0572147/" + position.coords.latitude + ",%20" + position.coords.longitude + "?callback=?";
-			console.log(url);
-
-			// Get weather data
-			$.getJSON(url, function(data) {
-				console.log(data.currently);
-				// Update page with weather data
-				$("#summary").text(data.currently.summary);
-				temperature = data.currently.apparentTemperature;
-				$("#temperature").html(data.currently.apparentTemperature + "<i class='wi wi-fahrenheit'></i>");
-			});
 		});
 	}
 
